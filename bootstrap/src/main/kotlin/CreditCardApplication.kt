f38f1577b78e56d065c1c009f405e7e17a592035
João Kiakumbo <jkiakumbo@icloud.com>
2025-10-22 22:08:46 +0100

    Add application entry point

---
+++ bootstrap/src/main/kotlin/CreditCardApplication.kt
@@ -0,0 +1,12 @@
+package dev.jkiakumbo.ccm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class CreditCardApplication
+
+fun main(args: Array<String>) {
+    runApplication<CreditCardApplication>(*args)
+}
